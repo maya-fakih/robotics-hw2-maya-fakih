d02578008a447232f3bed5686e876be88ffd01cc
maya-fakih <alfakihmaya1@gmail.com>
2026-07-06 10:54:05 +0300

    feat: implemented CookingRobot

---
+++ src/cooking_robot.cpp
@@ -0,0 +1,15 @@
+#include "cooking_robot.hpp"
+#include <stdexcept>
+
+CookingRobot::CookingRobot(const std::string& id, const std::string& name, int battery)
+    : Robot(id, name, battery) {}
+
+void CookingRobot::work() {
+    if (battery_ == 0)
+        throw std::runtime_error(name_ + " cannot work: battery is empty");
+    battery_ -= 15;
+    if (battery_ < 0) battery_ = 0;
+    status_ = "cooking";
+}
+
+std::string CookingRobot::type() const { return "CookingRobot"; }
